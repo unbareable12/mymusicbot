7ea8388d48c82a1fee0b96eef1f7241373a940f4
unbareable12 <102046861+unbareable12@users.noreply.github.com>
2022-03-21 21:10:58 +0545

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-        TOKEN: '', //write your discord bot token
+        TOKEN: 'OTE5NjA4NTI3MTYzNjMzNzA0.YbYSLg.wwbGuMhLkOT3fQDizNTYUfzPLPM', //write your discord bot token
         px: '!',
         playing: 'Umut Bayraktar ❤️ Music Bot',
 
